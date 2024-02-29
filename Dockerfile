0bd9d481a3b513c30547a2df8deb8acc62309246
rajdevmaurya <rajdevmaurya@gmail.com>
2024-02-29 16:48:51 +0530

    test

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM lolhens/baseimage-openjre
-ADD target/user-mgmt-app.jar user-mgmt-app.jar
-EXPOSE 80
-ENTRYPOINT ["java", "-jar", "user-mgmt-app.jar"]
--- src/main/java/com/demo/mgmtapp/constant/AppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.demo.mgmtapp.constant;
-
-
-public class AppConstant {
-
-    public static final String SUCCESS = "success";
-    public static final String FAIL = "fail";
-}
