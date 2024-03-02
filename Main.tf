960bfbb30452df72333a91e4c36a515c3e3ea147
rajdevmaurya <rajdevmaurya@gmail.com>
2024-03-02 14:56:51 +0530

    Updated

--- Main.tf
+++ Main.tf
@@ -1,19 +1,15 @@
-terraform {
-  required_version = ">= 1.1.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  cloud {
-    organization = "CloudQuickLabs"
-    workspaces {
-      name = "AzureLabs"
-    }
-  }
-}
-
+# ARM provider block -rekhu
 provider "azurerm" {
+  source = "hashicorp/azurerm"
+  version = "~> 3.0.2"
   features {}
+}
+# Terraform backend configuration block -precreated
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-cloudquickpocs"
+    storage_account_name = "ccpsazuretf0001"
+    container_name       = "ccpterraformstatefile"
+    key                  = "ccpsterraform.tfstate"
+  }
 }
