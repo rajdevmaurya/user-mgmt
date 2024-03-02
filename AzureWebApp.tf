0c6a337b3fd78547e485700c16cd3baf20d79570
rajdevmaurya <rajdevmaurya@gmail.com>
2024-03-02 19:04:28 +0530

    java

--- AzureWebApp.tf
+++ AzureWebApp.tf
@@ -37,7 +37,7 @@ resource "azurerm_app_service" "app-service" {
   app_service_plan_id = azurerm_app_service_plan.service-plan.id
 
   site_config {
-    linux_fx_version = "DOTNETCORE|3.1"
+    linux_fx_version = "JAVA|17"
   }
 
   tags = {
