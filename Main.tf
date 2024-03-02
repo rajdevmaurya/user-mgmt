6b395b37f17c32af6067c891785eb95640197eb8
rajdevmaurya <rajdevmaurya@gmail.com>
2024-03-02 14:47:03 +0530

    storage exp

--- Main.tf
+++ Main.tf
@@ -1,14 +1,19 @@
-# ARM provider block -rekhu
-provider "azurerm" {
-  version = "~>2.0"
-  features {}
-}
-# Terraform backend configuration block -precreated
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-cloudquickpocs"
-    storage_account_name = "ccpsazuretf0001"
-    container_name       = "ccpterraformstatefile"
-    key                  = "ccpsterraform.tfstate"
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
   }
+  cloud {
+    organization = "CloudQuickLabs"
+    workspaces {
+      name = "AzureLabs"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
