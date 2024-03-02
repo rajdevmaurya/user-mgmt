192760886feb9e39a20440ca8fadfc0c56841b61
rajdevmaurya <rajdevmaurya@gmail.com>
2024-03-02 10:42:30 +0530

    Update Azure_Storage_Account.tf

--- .github/workflows/Azure_Storage_Account.tf
+++ .github/workflows/Azure_Storage_Account.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "demo" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "myrgroup" {
+  name     = "rg-usr-mgmt"
   location = "East Us"
 }
 
 ##  Demo now
 resource "azurerm_storage_account" "MyStorageAccount" {
   name                     = "my-rg-007"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
+  resource_group_name      = azurerm_resource_group.myrgroup.name
+  location                 = azurerm_resource_group.myrgroup.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
